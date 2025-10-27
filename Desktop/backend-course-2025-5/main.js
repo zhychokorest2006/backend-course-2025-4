931b8e5c73fed63b3352ab1b3c4191b0a82a5a04
zhychokorest2006 bc2025-5 <zhychokorest2006@gmail.com>
2025-10-27 21:07:01 +0200

    Створено файл main.js

---
+++ Desktop/backend-course-2025-5/main.js
@@ -0,0 +1,21 @@
+import http from 'http';
+import { Command } from 'commander';
+import fs from 'fs/promises';
+import path from 'path';
+
+const program = new Command();
+program
+  .requiredOption('-h, --host <host>', 'Host address')
+  .requiredOption('-p, --port <port>', 'Port number')
+  .requiredOption('-c, --cache <path>', 'Cache directory');
+program.parse(process.argv);
+const options = program.opts();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Proxy server is running');
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}`);
+});
